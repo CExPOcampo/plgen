a58c249239f5fd6abe81254921b4f3b1328c06fe
CExPOcampo <pocampo@i-sight.com>
2018-02-01 11:06:41 -0500

    Added validation coloring for number fields if invalid.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import ExcelDataReducer from './excelDataReducer';
 import ColumnReducer from './columnReducer';
+import { reducer as formReducer } from 'redux-form';
 
 const allReducers = combineReducers({
 	excelData: ExcelDataReducer,
-	columns: ColumnReducer
+	columns: ColumnReducer,
+	form: formReducer
 });
 
 export default allReducers;
