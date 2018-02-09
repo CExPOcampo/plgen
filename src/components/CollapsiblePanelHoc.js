6c5ff2502a955ff41e502fa14916363f8f8001cc
CExPOcampo <pocampo@i-sight.com>
2018-02-09 11:58:15 -0500

    Added state to CollapsiblePanel HOC and added chevron (with direction based on state).

--- src/components/CollapsiblePanelHoc.js
+++ src/components/CollapsiblePanelHoc.js
@@ -3,16 +3,31 @@ import { Panel } from 'react-bootstrap';
 
 export default function asCollapsiblePanel(WrappedComponent) {
 	return class extends Component {
+		constructor(props) {
+			super(props);
+
+			this.state = {
+				open: true
+			}
+		}
 		render() {
 
 			const props = this.props;
 			const {namespace, title, titleName, ...passThroughProps} = props;
 
 			return (<div>
-				<Panel id={`${namespace}-${props.index}`} defaultExpanded>
+				<Panel id={`${namespace}-${props.index}`} expanded={this.state.open} onToggle={() => this.setState({open: !this.state.open})}>
 					<Panel.Heading>
 						<Panel.Title toggle>
-							<h3> {`${title}${titleName}`} </h3>
+
+							<h3>
+								{this.state.open ?
+									<span className="glyphicon glyphicon-chevron-down"/> :
+									<span className="glyphicon glyphicon-chevron-right"/>
+								}
+								{` ${title}${titleName}`}
+							</h3>
+
 						</Panel.Title>
 					</Panel.Heading>
 					<Panel.Collapse>
